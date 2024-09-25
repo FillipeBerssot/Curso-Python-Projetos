4ca3944b74843c29c34bd1e409f4cf938aee43b7
fillipeberssot <fillipeberssot95@hotmail.com>
2024-09-25 12:45:35 -0300

    Commit automático

--- Curso_Thiago/Exercicios/Lista_exercicio01/exercicio036.py
+++ Curso_Thiago/Exercicios/Lista_exercicio01/exercicio036.py
@@ -4,12 +4,46 @@
 #    Exemplo: se um video dura 1h , na velocidade 2 ele ira durar 30 min:
 
 
-def duracao_do_video_acelerado(tempo_do_video):
-    velocidades = [1.25, 1.5, 2.0]
-    for v in velocidades:
-        duracao_acelerada_do_video = tempo_do_video / v
-        print (f"Na velocidade {v}x, a duração do vídeo será de {duracao_acelerada_do_video:.2f} minutos.")
+def duracao_do_video_acelerado(tempo_do_video, opcao_de_velocidade_do_video):
+    partes = tempo_do_video.split(':')
+    horas = int(partes[0])
+    minutos = int(partes[1])
+    tempo_em_minutos = horas * 60 + minutos
+
+    velocidades = {
+        '1': 0.25,
+        '2': 0.5,
+        '3': 0.75,
+        '4': 1.25,
+        '5': 1.5,
+        '6': 1.75,
+        '7': 2.0
+    }
+
+    velocidade = velocidades[opcao_de_velocidade_do_video]
+    duracao_do_video_acelerado = tempo_em_minutos / velocidade
+    print(f'\nNa velocidade {velocidade}. A duração do vídeo será de {duracao_do_video_acelerado:.2f} minutos.')
 
 
-tempo_do_video = float(input('Digite aqui o tempo de video do youtube que voçê irá assitir em minutos: '))
-duracao_do_video_acelerado(tempo_do_video)
+tempo_do_video = input('Digite aqui o tempo de video do youtube que voçê irá assitir (00:00): ')
+
+opcoes = """
+Escolha a velocidade do vídeo:
+[1] para velocidade 0.25
+[2] para velocidade 0.5
+[3] para velocidade 0.75
+[4] para velocidade 1.25
+[5] para velocidade 1.5
+[6] para velocidade 1.75
+[7] para velocidade 2.0
+"""
+
+opcao_de_velocidade_do_video = ''
+while opcao_de_velocidade_do_video not in ['1', '2', '3', '4', '5', '6', '7']:
+    print(opcoes)
+    opcao_de_velocidade_do_video = input('Digite aqui sua opcão de velocidade de acordo com as opções acima: ').strip()
+
+    if opcao_de_velocidade_do_video not in ['1', '2', '3', '4', '5', '6', '7']:
+        print('Opção inválida. Por favor, escolha uma opção entre 1 e 7.')
+
+duracao_do_video_acelerado(tempo_do_video, opcao_de_velocidade_do_video)
